a9eb9d01da58958fb1194e46ebbc9b6085d35e9b
vandana182 <119412803+vandana182@users.noreply.github.com>
2022-11-29 16:46:26 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -9,6 +9,15 @@ public class Main {
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
+    for(int i: cycles)
+    {
+      if(i<410)
+        counts.lowCount++;
+      else if(i>=410 && i<=949)
+        counts.mediumCount++;
+      else
+        counts.highCount++;
+    }
     return counts;
   }
 
